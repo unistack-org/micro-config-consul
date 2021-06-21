a3438bcc2f618a07c8d8a4a7c13227d48b5f67f4
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-06-21 16:03:21 +0300

    update
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- consul.go
+++ consul.go
@@ -82,7 +82,7 @@ func (c *consulConfig) Init(opts ...config.Option) error {
 	return nil
 }
 
-func (c *consulConfig) Load(ctx context.Context) error {
+func (c *consulConfig) Load(ctx context.Context, opts ...config.LoadOption) error {
 	for _, fn := range c.opts.BeforeLoad {
 		if err := fn(ctx, c); err != nil && !c.opts.AllowFail {
 			return err
@@ -97,11 +97,20 @@ func (c *consulConfig) Load(ctx context.Context) error {
 	}
 
 	if err == nil && pair != nil {
+		options := config.NewLoadOptions(opts...)
+		mopts := []func(*mergo.Config){mergo.WithTypeCheck}
+		if options.Override {
+			mopts = append(mopts, mergo.WithOverride)
+		}
+		if options.Append {
+			mopts = append(mopts, mergo.WithAppendSlice)
+		}
+
 		src, err := rutil.Zero(c.opts.Struct)
 		if err == nil {
 			err = c.opts.Codec.Unmarshal(pair.Value, src)
 			if err == nil {
-				err = mergo.Merge(c.opts.Struct, src, mergo.WithOverride, mergo.WithTypeCheck, mergo.WithAppendSlice)
+				err = mergo.Merge(c.opts.Struct, src, mopts...)
 			}
 		}
 
@@ -120,7 +129,7 @@ func (c *consulConfig) Load(ctx context.Context) error {
 	return nil
 }
 
-func (c *consulConfig) Save(ctx context.Context) error {
+func (c *consulConfig) Save(ctx context.Context, opts ...config.SaveOption) error {
 	for _, fn := range c.opts.BeforeSave {
 		if err := fn(ctx, c); err != nil && !c.opts.AllowFail {
 			return err
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/unistack-org/micro-config-consul/v3
 
-go 1.15
+go 1.16
 
 require (
 	github.com/google/btree v1.0.0 // indirect
@@ -9,5 +9,5 @@ require (
 	github.com/hashicorp/golang-lru v0.5.3 // indirect
 	github.com/imdario/mergo v0.3.12
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/unistack-org/micro/v3 v3.3.17
+	github.com/unistack-org/micro/v3 v3.4.0
 )
